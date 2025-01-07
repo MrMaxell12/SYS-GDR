3e371dca192860253d12ba5f678792372d2545d1
Sevenings <lvlassis.2@gmail.com>
2025-01-06 22:23:32 -0300

    reestruturado o projeto

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from app import app
+from . import main
 from flask import render_template
 
 
@@ -6,32 +6,32 @@ from flask import render_template
 ------------------------------------------""" 
 
 # Página "Portal"
-@app.route('/')
+@main.route('/')
 def index_route():
     return render_template('index.html')
 
 # Página de Login
-@app.route('/login', methods=['GET'])
+@main.route('/login', methods=['GET'])
 def login_route():
     return render_template('login.html')
 
 # Verificação de Login
-@app.route('/login', methods=['POST'])
+@main.route('/login', methods=['POST'])
 def login_verify():
-    return 
+    return 'hello'
 
 # Página Principal do Usuário
-@app.route('/main')
+@main.route('/main')
 def main_route():
     return render_template('main.html')
 
 # Página de Status
-@app.route('/status')
+@main.route('/status')
 def status_route():
     return render_template('status.html')
 
 # Página de Inventário
-@app.route('/inventory')
+@main.route('/inventory')
 def inventory_route():
     return render_template('inventory.html')
 
